3416218a6696db82ff2b7a2ec478190d1b0f96de
Aktan Azat <aazat@ucdavis.edu>
2026-01-29 10:33:23 -0800

    Initial commit

---
+++ Debug/Adafruit_OLED.d
@@ -0,0 +1,87 @@
+# FIXED
+
+Adafruit_OLED.obj: ../Adafruit_OLED.c
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+Adafruit_OLED.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_types.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_memmap.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_common_reg.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_ints.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/gpio.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/spi.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_map.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_patch.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/utils.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/prcm.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/uart.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/interrupt.h
+Adafruit_OLED.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/uart_if.h
+Adafruit_OLED.obj: ../pin_mux_config.h
+Adafruit_OLED.obj: ../Adafruit_SSD1351.h
+Adafruit_OLED.obj: ../Adafruit_GFX.h
+Adafruit_OLED.obj: ../glcdfont.h
+
+../Adafruit_OLED.c:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_types.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_memmap.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_common_reg.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_ints.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/gpio.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/spi.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_map.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_patch.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/utils.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/prcm.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/uart.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/interrupt.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/uart_if.h:
+
+../pin_mux_config.h:
+
+../Adafruit_SSD1351.h:
+
+../Adafruit_GFX.h:
+
+../glcdfont.h:
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../cc3200v1p32.cmd 
+
+C_SRCS += \
+../Adafruit_GFX.c \
+../Adafruit_OLED.c \
+../i2c_if.c \
+../main.c \
+../oled_tests.c \
+../pin_mux_config.c \
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/startup_ccs.c \
+../uart_if.c 
+
+C_DEPS += \
+./Adafruit_GFX.d \
+./Adafruit_OLED.d \
+./i2c_if.d \
+./main.d \
+./oled_tests.d \
+./pin_mux_config.d \
+./startup_ccs.d \
+./uart_if.d 
+
+OBJS += \
+./Adafruit_GFX.obj \
+./Adafruit_OLED.obj \
+./i2c_if.obj \
+./main.obj \
+./oled_tests.obj \
+./pin_mux_config.obj \
+./startup_ccs.obj \
+./uart_if.obj 
+
+OBJS__QUOTED += \
+"Adafruit_GFX.obj" \
+"Adafruit_OLED.obj" \
+"i2c_if.obj" \
+"main.obj" \
+"oled_tests.obj" \
+"pin_mux_config.obj" \
+"startup_ccs.obj" \
+"uart_if.obj" 
+
+C_DEPS__QUOTED += \
+"Adafruit_GFX.d" \
+"Adafruit_OLED.d" \
+"i2c_if.d" \
+"main.d" \
+"oled_tests.d" \
+"pin_mux_config.d" \
+"startup_ccs.d" \
+"uart_if.d" 
+
+C_SRCS__QUOTED += \
+"../Adafruit_GFX.c" \
+"../Adafruit_OLED.c" \
+"../i2c_if.c" \
+"../main.c" \
+"../oled_tests.c" \
+"../pin_mux_config.c" \
+"/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/startup_ccs.c" \
+"../uart_if.c" 
+
+
